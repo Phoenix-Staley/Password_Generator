784c6b8d452a34c4d6295f05d9e7b87e86ff0149
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-03-08 15:06:18 -0800

    Reformatted file arrangement, and adjusted comments

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var errorMessage = document.querySelector("#error");
 
 // Write password to the #password input
 function writePassword() {
@@ -13,67 +14,55 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// 1. Define arrays of lower case, upper case, numbers, and specials ✅
+// These are each character set the user can choose to include
 var lowerCases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCases = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specials = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
 function generatePassword() {
-  // 1. Make an empty array ✅
-  // 2. Make an empty string ✅
-  // 3. Make a variable for the password length ✅
-  // 4. Get user inputs through prompts and confirms ✅
-  // 5. Check to make sure prompt input is valid ✅
-  // 6. Push the constant arrays into the empty array ✅
-  // 7. Repeat through each chosen array once, push a random character to the string ✅
-  // 8. Run a for loop, randomly selecting an approved array and a random character in that array ✅
-  // 9. Push each of those characters to the password string ✅
-  // 10. Return the password string when the for loops are done ✅
-
-  // These variables will hold the character sets the user selects, and the password string respectively
+  // These variables will hold the character sets the user selects, the password, and the password length
   var selectedChars = [];
   var password = "";
   var passwordLength = 8;
+
   var lengthInput = prompt("Please enter the length of your password. (Default: 8)", "Enter a number");
 
-  if (isNaN(lengthInput)) {
-    console.log("Input is not a number")
-    alert("Please input a number.");
-  } else if (lengthInput >= 8 && lengthInput <= 128) {
-    passwordLength = lengthInput;
-    console.log("Password length: " + passwordLength);
-  } else {
-    console.log("Input is out of acceptable range.");
-    alert("Please enter a number between 8 and 128.");
+  // This checks if the length input is valid and reacts accordingly
+  var lengthGiven = false;
+  while (!lengthGiven) {
+    if (isNaN(lengthInput)) {
+      alert("Please input a number.");
+      lengthInput = prompt("Please enter the length of your password. (Default: 8)", "Enter a number");
+    } else if (lengthInput >= 8 && lengthInput <= 128) {
+      passwordLength = lengthInput;
+      lengthGiven = true;
+      console.log("Password length: " + passwordLength);
+    } else {
+      alert("Please enter a number between 8 and 128.");
+      lengthInput = prompt("Please enter the length of your password. (Default: 8)", "Enter a number");
+    }
   }
 
+  // These prompt the user to select the character sets they do/don't want to use
+  // Pushing the corresponding character set to the selectedChars array
   if (confirm("Press \"OK\" to confirm lower case letters")) {
     selectedChars.push(lowerCases);
-  } else {
-    console.log("Lower case letters declined.");
   }
-
   if (confirm("Press \"OK\" to confirm upper case letters")) {
     selectedChars.push(upperCases);
-  } else {
-    console.log("Upper case letters declined.");
   }
-
   if (confirm("Press \"OK\" to confirm numbers")) {
     selectedChars.push(numbers);
-  } else {
-    console.log("Numbers declined.");
   }
-
   if (confirm("Press \"OK\" to confirm special characters")) {
     selectedChars.push(specials);
-  } else {
-    console.log("Special characters declined.");
   }
+  console.log(selectedChars);
 
+  // This checks to make sure at least 1 character set was selected
   if (selectedChars.length === 0) {
-    alert("You must select at least 1 character set.");
+    alert("You must select at least 1 character set. Please press the \"Generate Password\" button again.");
   } else {
     console.log("Selected characters:")
     console.log(selectedChars);
@@ -86,7 +75,7 @@ function generatePassword() {
     password += currentArray[randomNum];
   }
 
-  // This places random characters from a random selected character type until the password is as the appropriate length
+  // This pushes a random character from a random set onto the password string until the password is at the appropriate length
   for(let i = password.length; i < passwordLength; i++) {
     randomArr = selectedChars[Math.floor(Math.random() * selectedChars.length)];
     randomChar = randomArr[Math.floor(Math.random() * randomArr.length)];
